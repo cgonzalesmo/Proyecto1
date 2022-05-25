aac1e23430d6f84711e2a6b81b5be83f9d938f2e
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-05-25 01:14:21 -0500

    edit Manifest

--- app/src/main/java/com/example/proyecto1/ui/theme/Theme.kt
+++ app/src/main/java/com/example/proyecto1/ui/theme/Theme.kt
@@ -5,34 +5,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = PrimaryDark,
+    primaryVariant = SecondaryPrimaryDark,
+    secondary = MyBlue
 )
 
 @Composable
-fun Proyecto1Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+fun Proyecto1Theme(darkTheme: Boolean = true, content: @Composable () -> Unit) {
+    val colors = DarkColorPalette
+
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = if (darkTheme) PrimaryDark else Color.White
+        )
     }
 
     MaterialTheme(
