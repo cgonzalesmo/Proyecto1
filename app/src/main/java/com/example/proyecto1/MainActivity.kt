83087f0b2a4173fe8879416145369e79bad9a8cf
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-05-25 01:00:53 -0500

    add MainActivity

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -3,17 +3,54 @@ package com.example.proyecto1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.proyecto1.screens.NavGraphs
+import com.example.proyecto1.screens.common.StandardScaffold
+import com.example.proyecto1.ui.theme.PrimaryDark
 import com.example.proyecto1.ui.theme.Proyecto1Theme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.rememberNavHostEngine
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Proyecto1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = PrimaryDark
+                ) {
+                    val navController = rememberNavController()
+                    val navHostEngine = rememberNavHostEngine()
+
+                    StandardScaffold(
+                        navController = navController
+                    ) {
+                        Box(modifier = Modifier.padding(it)) {
+                            DestinationsNavHost(
+                                navGraph = NavGraphs.root,
+                                navController = navController,
+                                engine = navHostEngine
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Proyecto1Theme {
@@ -39,5 +76,5 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     Proyecto1Theme {
         Greeting("Android")
-    }
+    }*/
 }
